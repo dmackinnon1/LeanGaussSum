62278f65719ca0ca1738f056a45bdda59526ce0b
Dan MacKinnon <dmackinnon1@gmail.com>
2026-09-12 17:27:30 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «gauss» where
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.7.0"
 
 @[default_target]
 lean_lib «Gauss» where
